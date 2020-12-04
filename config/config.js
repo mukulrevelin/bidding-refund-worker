6167ec393ff9f555748932cd60ef3860d225f637
deeptxfinite <62990536+deeptxfinite@users.noreply.github.com>
2020-12-04 23:17:17 +0530

    balance reward api changed]

--- config/config.js
+++ config/config.js
@@ -30,6 +30,13 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    redisUrl: 'redis://admin:2WmdlyTdgx033KPMZOdPwGD8F9D1APAGjoeTRuKX5KU=@mzaalo-prod.redis.cache.windows.net:6379/0',
+    auth_secret_key: 'zHdguH0VPpPTJzCftRZelObX',
+    admin_auth_secret_key: 'JkYXRhIjoiMSIsImlhdCI6MTU1M',
+    balance: {
+      url: 'http://reward-engine:56140/api/v1/rewards/',
+      ocpApimSubscriptionKey: '1d0caac2702049b89a305929fdf4cbae'
+    }
   }
 }
